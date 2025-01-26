191909e248ffad0a7a08a9dca25a584721b6d01b
KarimaFathi <karimafathifadili@gmail.com>
2025-01-26 22:30:38 +0100

    Add Exo45

--- Exo45/Exo45.cpp
+++ Exo45/Exo45.cpp
@@ -3,51 +3,95 @@
 #include <vector>
 using namespace std;
 
+struct stClientInfo {
+	string accountNumber;
+	short pinCode;
+	string name;
+	string phoneNumber;
+	float accountBalance;
+};
+
+stClientInfo readClientInfo(stClientInfo &clientInfo) {
+	cout << "Please enter your account number ? ";
+	cin >> clientInfo.accountNumber;
+	cout << "Please enter your pin code ? ";
+	cin >> clientInfo.pinCode;
+	cin.ignore();
+	cout << "Please enter your name ? ";
+	getline(cin, clientInfo.name);
+	cout << "Please enter your phone number ? ";
+	cin >> clientInfo.phoneNumber;
+	cout << "Please enter your account balance ? ";
+	cin >> clientInfo.accountBalance;
+	return clientInfo;
+}
+
+string convertRecordToStringWithDelim(stClientInfo clientInfo, string delim) {
+	string clientRecord = "";
+	clientRecord += clientInfo.accountNumber + delim;
+	clientRecord += to_string(clientInfo.pinCode) + delim;
+	clientRecord += clientInfo.name + delim;
+	clientRecord += clientInfo.phoneNumber + delim;
+	clientRecord += to_string(clientInfo.accountBalance);
+	return clientRecord;
+}
+
 
 //method 1
- vector<string> readPersonRecords(vector<string> &vPersonInfo) {
-	 int accountNumber = 0;
-	 short pinCode = 0;
-	 string name = "";
-	 int phoneNumber = 0;
-	 float accountBalance = 0;
-	cout << "Please enter your account number ? ";
-	cin >> accountNumber;
-	vPersonInfo.push_back(to_string(accountNumber));
-	cout << "Please enter your pin code ? ";
-	cin >> pinCode;
-	vPersonInfo.push_back(to_string(pinCode));
-	cout << "Please enter your name ? ";
-	cin >> name;
-	vPersonInfo.push_back(name);
-	cout << "Please enter your phone number ? ";
-	cin >> phoneNumber;
-	vPersonInfo.push_back(to_string(phoneNumber));
-	cout << "Please enter your account balance ? ";
-	cin >> accountBalance;
-	vPersonInfo.push_back(to_string(accountBalance));
-	return vPersonInfo;
-}
+// vector<string> readPersonRecords(vector<string> &vPersonInfo) {
+//	 int accountNumber = 0;
+//	 short pinCode = 0;
+//	 string name = "";
+//	 int phoneNumber = 0;
+//	 float accountBalance = 0;
+//	cout << "Please enter your account number ? ";
+//	cin >> accountNumber;
+//	vPersonInfo.push_back(to_string(accountNumber));
+//	cout << "Please enter your pin code ? ";
+//	cin >> pinCode;
+//	vPersonInfo.push_back(to_string(pinCode));
+//	cout << "Please enter your name ? ";
+//	cin >> name;
+//	vPersonInfo.push_back(name);
+//	cout << "Please enter your phone number ? ";
+//	cin >> phoneNumber;
+//	vPersonInfo.push_back(to_string(phoneNumber));
+//	cout << "Please enter your account balance ? ";
+//	cin >> accountBalance;
+//	vPersonInfo.push_back(to_string(accountBalance));
+//	return vPersonInfo;
+//}
+//
+//
+//string convertRecordToStringWithDelim(vector<string>  vPersonInfo, string delim) {
+//	string s1 = "";
+//	for (string &info : vPersonInfo) {
+//		s1 += info + delim;
+//	}
+//	for (short i = 0; i < delim.length(); i++) {
+//		s1.pop_back();
+//	}
+//	return s1;
+//}
 
 
-string convertRecordToStringWithDelim(vector<string>  vPersonInfo, string delim) {
-	string s1 = "";
-	for (string &info : vPersonInfo) {
-		s1 += info + delim;
-	}
-	for (short i = 0; i < delim.length(); i++) {
-		s1.pop_back();
-	}
-	return s1;
-}
+
 
 
 int main()
 {
-	vector<string>  vPersonInfo;
+	stClientInfo clientInfo;
+	readClientInfo(clientInfo);
+	cout << endl << "Client record for saving is: " << endl;
+	cout << endl << convertRecordToStringWithDelim(clientInfo, "#//#");
+
+
+
+
+	/*vector<string>  vPersonInfo;
 	readPersonRecords(vPersonInfo);
 	cout << endl << "Client record for saving is: " << endl;
-	cout << endl << convertRecordToStringWithDelim(vPersonInfo, "#//#");
+	cout << endl << convertRecordToStringWithDelim(vPersonInfo, "#//#");*/
 
 
 }
